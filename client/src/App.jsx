11081c7d37af580649e070109ef095a346c36284
Deepan Kumar <deepanrvdvenu@gmail.com>
2023-11-21 19:06:00 +0530

    completed signu; page functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import About from "./pages/About";
-import Profile from "./pages/Profile";
-import SignIn from "./pages/Signin";
-import SignUp from "./pages/SignUp";
-import Header from "./components/Header";
+import Home from "./pages/Home.jsx";
+import About from "./pages/About.jsx";
+import Profile from "./pages/Profile.jsx";
+import SignIn from "./pages/Signin.jsx";
+import SignUp from "./pages/SignUp.jsx";
+import Header from "./components/Header.jsx";
 export default function App() {
   return (
     <BrowserRouter>
-    <Header/>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/sign-in" element={<SignIn />} />
