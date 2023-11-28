5ffac8f6905120ae18559725937d206f299691b4
Deepan Kumar <deepanrvdvenu@gmail.com>
2023-11-28 20:18:22 +0530

    added signout functionality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -69,3 +69,14 @@ export const google = async (req, res, next) => {
     next(err);
   }
 };
+
+
+export const signOut = async(req,res)=>{
+  try{
+    res.clearCookie("access_token");
+    res.status(200).json("User has been logged Out");
+  }
+  catch(error){
+    next(error);
+  }
+}
