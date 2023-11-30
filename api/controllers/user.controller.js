8b4d88db9ad060760741c6b4a66824bfae8e88e3
Deepan Kumar <deepanrvdvenu@gmail.com>
2023-12-01 04:04:02 +0530

    completed user listing fn

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,6 +1,8 @@
+import Listing from "../models/listing.model.js";
 import User from "../models/user.model.js";
 import { errorHandler } from "../utils/error.js";
 import bcryptjs from "bcryptjs";
+
 export const test = (req, res) => {
   res.json({
     message: "Hkkkiiiii",
@@ -43,5 +45,18 @@ export const deleteUser = async (req, res, next) => {
     res.status(200).json("User has been deleted");
   } catch (err) {
     next(err);
+  }
+};
+
+export const getUserListings = async (req, res, next) => {
+  if (req.user.id === req.params.id) {
+    try {
+      const listings = await Listing.find({ userRef: req.params.id });
+      res.status(200).json(listings);
+    } catch (error) {
+      next(error);
+    }
+  } else {
+    return next(errorHandler(401, "You can only view your own listings!"));
   }
 };
