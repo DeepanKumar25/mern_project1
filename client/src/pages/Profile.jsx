984e10b8ef8ca6ccb996d89eee9915d3efd488e8
Deepan Kumar <deepanrvdvenu@gmail.com>
2023-12-01 18:24:34 +0530

    created updatelisting api

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -138,6 +138,24 @@ export default function Profile() {
     }
   };
 
+  const handleListingDelete = async (listingId) => {
+    try {
+      const res = await fetch(`/api/listing/delete/${listingId}`, {
+        method: "DELETE",
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        console.log(data.message);
+        return;
+      }
+      setUserListings((prev) =>
+        prev.filter((listing) => listing._id !== listingId)
+      );
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
@@ -234,7 +252,9 @@ cursor-pointer self-center mt-2"
 
       {userListings && userListings.length > 0 && (
         <div className="flex flex-col gap-4">
-          <h1 className="text-center mt-7  text-2xl font-semibold">Your Listings</h1>
+          <h1 className="text-center mt-7  text-2xl font-semibold">
+            Your Listings
+          </h1>
           {userListings.map((listing) => (
             <div
               key={listing._id}
@@ -256,6 +276,7 @@ cursor-pointer self-center mt-2"
 
               <div className="flex flex-col item-center">
                 <button
+                  onClick={() => handleListingDelete(listing._id)}
                   className="text-red-700
 uppercase"
                 >
