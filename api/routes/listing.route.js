9e0190b9d3b076886689e5564884e5362a35b04c
Deepan Kumar <deepanrvdvenu@gmail.com>
2023-12-03 03:39:51 +0530

    contact functionality

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -3,7 +3,7 @@ import {
   createListing,
   deleteListing,
   updateListing,
-  getListing
+  getListing,
 } from "../controllers/listing.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
@@ -11,6 +11,6 @@ const router = express.Router();
 
 router.post("/create", verifyToken, createListing);
 router.delete("/delete/:id", verifyToken, deleteListing);
-router.post("/update/:id",verifyToken,updateListing);
-router.get("/get/:id",getListing);
+router.post("/update/:id", verifyToken, updateListing);
+router.get("/get/:id", getListing);
 export default router;
